0e7e4cb7d4b5d78e8068ce270e8b84e8c02a9871
kamila <kamilalimamf@gmail.com>
2021-09-04 10:54:59 -0300

    exercicio-semana3-kamila

---
+++ farmaciakamila.js
@@ -0,0 +1,18 @@
+var produto1 = Numero (prompt("Insira o código do primeiro produto: "));
+var produto2 = Numero (prompt("Insira o codigo do segundo produto: "));
+if (poduto1 == produto2) {
+    alert("Farmácia JS está em promoção. A cada dois produtos iguais comprados, receba 5 reais de desconto. Aproveite!");
+    alert("Você ganhou um desconto de 5 reais na sua compra: ");
+var total = Number(prompt("Digite o valor total da sua compra: "));
+Totalfinal = total - 5;
+    alert("O valor da sua compra com desconto é de: R$" + Totalfinal)
+
+} else {
+    alert ("Obrigada por comprar conosco!");
+}
+ 
+ 
+
+
+
+ 
